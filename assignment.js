ebc9d03d62fb75976cfaaa41b1b5be4dde12e5d7
acrosshorizon <80151195+acrosshorizon@users.noreply.github.com>
2022-05-03 19:16:36 +0700

    Update assignment.js

--- assignment.js
+++ assignment.js
@@ -3,23 +3,20 @@ def best_revenue(revenue, travel_days, start):
     numCity = len(revenue[0])    #numColumn
     numDays= len(revenue) #numRow
     dp = [-1]*numCity
-    findBestRevenue(revenue, travel_days, start, numDays, total_revenue, dp)
+    total_revenue = findBestRevenue(revenue, travel_days, start, numDays, dp)
 
 
-def findBestRevenue(revenue, travel_days, start, numDays, total_revenue, dp):
+def findBestRevenue(revenue, travel_days, start, numDays, dp):
     if start == numDays:
-        return total_revenue
+        return 0
     max = 0
     if dp[start] != -1:
         return dp[start]
     for day in travel_days[start]:
         if travel_days[start][day] != -1:
-            if revenue[start][day] + findBestRevenue(revenue, travel_days, start + travel_days[start][day], numDays, total_revenue, dp) > findBestRevenue(revenue, travel_days, start + travel_days[start][day] - 1, numDays, total_revenue, dp):
-                max = revenue[start][day] + findBestRevenue(revenue, travel_days, start + travel_days[start][day], numDays, total_revenue, dp)
-            else:
-                max = findBestRevenue(revenue, travel_days, start + travel_days[start][day] - 1, numDays, total_revenue, dp)
+            max = max(revenue[start][day] + findBestRevenue(revenue, travel_days, start + travel_days[start][day], numDays, dp), findBestRevenue(revenue, travel_days, start + travel_days[start][day] - 1, numDays, dp))
         else:
-            max = findBestRevenue(revenue, travel_days, start + travel_days[start][day] - 1, numDays, total_revenue, dp)
+            max = findBestRevenue(revenue, travel_days, start + travel_days[start][day] - 1, numDays, dp)
     dp[start] = max
     return dp[start]
 
